24da2df28c2760da33e8459afa7b84da559c89ae
jsh9 <jian.sh.7@gmail.com>
2017-11-11 11:32:39 -0800

    updated docs of get_color(), added MD file

--- plot_utils.py
+++ plot_utils.py
@@ -25,7 +25,7 @@ def piechart(target_array, class_names=None, fig=None, ax=None,
              figsize=(3,3), dpi=100, colors=None, autopct='%1.1f%%',
              fontsize=None, **piechart_kwargs):
     '''
-    Plot a pie chart demonstrating proportions of different categories within 
+    Plot a pie chart demonstrating proportions of different categories within
     an array.
 
     [Parameters]
@@ -282,7 +282,8 @@ def get_colors(N,color_scheme='tab10'):
         Number of qualitative colors desired. If None, returns all the colors
         in the specified color scheme.
     color_scheme : <str> or {0, 10, 8.3, 8.4}
-        Color scheme specifier. Valid str names are:
+        Color scheme specifier. Valid specifiers are:
+        (1) Matplotlib qualitative color map names:
             'Pastel1'
             'Pastel2'
             'Paired'
@@ -295,6 +296,14 @@ def get_colors(N,color_scheme='tab10'):
             'tab20'
             'tab20b'
             'tab20c'
+            ![](https://matplotlib.org/mpl_examples/color/colormaps_reference_04.png)
+        (2) 8.3 and 8.4 (floats): old and new MATLAB color scheme
+            Old:
+            ![](https://www.mathworks.com/help/matlab/graphics_transition/transition_colororder_old.png)
+            New:
+            ![](https://www.mathworks.com/help/matlab/graphics_transition/transition_colororder.png)
+        (3) 0 and 10 (ints):
+            0 is equivalent to old MATLAB scheme; 10 is equivalent to 'tab10'.
 
     [Returns]
     A list of colors.
