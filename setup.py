3b72b73ec25cbf2ebca2b99cd457703ba007540f
jsh9 <jian.sh.7@gmail.com>
2018-08-26 15:40:00 -0700

    Modify package_data

--- setup.py
+++ setup.py
@@ -22,5 +22,6 @@ setup(
                       'matplotlib',
     ],
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
-    package_data = {'shapefiles': ['*']}
+    package_data = {'shapefiles/usa_counties': ['*'],
+                    'shapefiles/usa_states': ['*']}
 )
