c486270450571efc6b54e78bc74d9f76b6278f85
Thomas Eiband <thomas.eiband@dlr.de>
2020-11-02 20:40:12 +0100

    add flexibility to get_ax3d

--- plot_utils/extensions/axis3d.py
+++ plot_utils/extensions/axis3d.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 
 
-def get_ax3d(new_figure=True, figsize=(4, 3), elev=45, azim=45):
+def get_ax3d(figure=None, subplot=111, figsize=(4, 3), elev=45, azim=45):
     """
     Get axis for a 3d plot
-    :param new_figure: open new figure
+    :param figure: figure handle or open a new figure with None
     :param figsize:
     :param elev: elevation of camera view (degree)
     :param azim: azimut value of camera view (degree)
     :return:
     """
-    if new_figure:
-        fig = plt.figure(figsize=figsize)
-    ax = fig.add_subplot(111, projection='3d')
+    if not figure:
+        figure = plt.figure(figsize=figsize)
+    ax = figure.add_subplot(subplot, projection='3d')
     ax.view_init(elev, azim)
     return ax
