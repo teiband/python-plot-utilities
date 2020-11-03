dadd35d2bc0a95335eccf0aa0c85b5c220a0b568
Thomas Eiband <thomas.eiband@dlr.de>
2020-11-03 08:19:42 +0100

    add python version check

--- plot_utils/__init__.py
+++ plot_utils/__init__.py
@@ -4,16 +4,16 @@ from .helper import *
 from .helper import _find_axes_lim
 from .helper import _calc_r2_score
 
-"""
-from .colors_and_lines import *
-from .image import *
-from .maps import *
-from .misc import *
-from .multiple_columns import *
-from .one_column import *
-from .time_series import *
-from .two_columns import *
+import sys
+if sys.version_info.major == 3:
+    from .colors_and_lines import *
+    from .image import *
+    from .maps import *
+    from .misc import *
+    from .multiple_columns import *
+    from .one_column import *
+    from .time_series import *
+    from .two_columns import *
 
-from .maps import _translate_state_abbrev
-from .maps import _convert_FIPS_to_state_name
-"""
+    from .maps import _translate_state_abbrev
+    from .maps import _convert_FIPS_to_state_name
